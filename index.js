913311f7ed55a520dbf048e1db2de31f63f89b7b
shahab hamidi <shahabhamidi73@gmail.com>
2025-12-04 14:01:18 +0430

    Update index.js

--- index.js
+++ index.js
@@ -399,6 +399,11 @@ async function loadAndSyncDataForPage(page) {
         await syncRequestsWithGoogleSheets(allData);
         break;
       case 'requests':
+      case 'request-menu':
+        await syncEmployeesWithGoogleSheets(allData);
+        await syncMotorcyclesWithGoogleSheets(allData); // برای depts
+        await syncRequestsWithGoogleSheets(allData);
+        break;
       case 'history':
       case 'motorcycle-status':
         await syncMotorcyclesWithGoogleSheets(allData); // برای depts
@@ -1790,9 +1795,6 @@ function toggleUserDropdown() {
   dropdown.classList.toggle('hidden');
 }
 document.addEventListener('DOMContentLoaded', initApp);
-if (window.location.hostname !== '127.0.0.1' && window.location.hostname !== 'localhost') {
-  (function () { function c() { var b = a.contentDocument || a.contentWindow.document; if (b) { var d = b.createElement('script'); d.innerHTML = "window.__CF$cv$params={r:'99bbf8eb8072d381',t:'MTc2MjY3NzI4MC4wMDAwMDA='};var a=document.createElement('script');a.nonce='';a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);"; b.getElementsByTagName('head')[0].appendChild(d) } } if (document.body) { var a = document.createElement('iframe'); a.height = 1; a.width = 1; a.style.position = 'absolute'; a.style.top = 0; a.style.left = 0; a.style.border = 'none'; a.style.visibility = 'hidden'; document.body.appendChild(a); if ('loading' !== document.readyState) c(); else if (window.addEventListener) document.addEventListener('DOMContentLoaded', c); else { var e = document.onreadystatechange || function () { }; document.onreadystatechange = function (b) { e(b); 'loading' !== document.readyState && (document.onreadystatechange = e, c()) } } } })();
-}
 function filterRequests(filter) {
   currentRequestFilter = filter;
   document.querySelectorAll('[id^="filter-request-"]').forEach(btn => btn.classList.remove('active-filter'));
